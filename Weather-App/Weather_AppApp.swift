47e608d97cd1be4853eb7aa2ac618aaf9ca199c8
Trungkun103 <trungkun103@gmail.com>
2025-11-07 16:06:41 +0700

    checked

---
+++ Weather-App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather-App
+//
+//  Created by TrungAnhx on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
